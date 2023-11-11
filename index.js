1fc81ca72b90a86544d8324456a76fc89e91763e
Joseph <JosephSped23@gmail.com>
2023-11-11 10:16:20 -0500

    Completed array filter method

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution here
+// function filter(collection) {
+//     for (const user of collection) {
+//       if (findMatching(drivers)) {
+//         console.log(Drivers.firstName);
+//       }
+//     }
+// }
+// function findMatching(drivers, name){
+//     return (drivers, name)
+// }
+
+
+//
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+ const findDriver = drivers.filter(driver => driver.toLowerCase() === 
+ name.toLowerCase());
+    return [...(findDriver)];
+}
+
+
+console.log (findMatching(drivers, 'Bobby'));
+
+function fuzzyMatch(drivers, providedLetters){
+    const providedMatch = drivers.filter(driver => driver.slice(0, 2) === 
+    providedLetters);
+       return [...(providedMatch)];
+   }
+   
+   
+   console.log (fuzzyMatch(drivers, 'Sa'));
+
+    function matchName (drivers, name){
+        const checkName = drivers.filter(driver => driver.name === name);
+        return checkName;
+        
+    }
+
